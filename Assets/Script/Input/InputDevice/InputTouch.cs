9e283e9f061afeb633e5216491d38a97b608abdc
BernardTheFour <bernardctxy1@gmail.com>
2020-10-28 23:36:26 +0700

    add touch input

--- Assets/Script/Input/InputDevice/InputTouch.cs
+++ Assets/Script/Input/InputDevice/InputTouch.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class InputTouch : MonoBehaviour
 {
-    private Vector2 TouchCoordinate;
-    private float SwipeThreesold = 20f;
+    private Vector2 InitialTouchCoordinate;
+    private Vector2 CurrentTouchCoordinate;
+    private Vector2 FinalTouchCoordinate;
+    private Touch myTouch;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float SwipeThreesold = 20f;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.touchCount > 0)
+        {
+            myTouch = Input.GetTouch(0);
 
+            switch (myTouch.phase)
+            {
+                case TouchPhase.Began:
+                    InitialTouchCoordinate = myTouch.position;
+                    Debug.Log(myTouch + " is began");
+                    break;
+                case TouchPhase.Moved:
+                    CurrentTouchCoordinate = myTouch.position;
+                    Debug.Log(myTouch + " is moving to " + CurrentTouchCoordinate);
+                    break;
+                case TouchPhase.Ended:
+                    FinalTouchCoordinate = myTouch.position;
+
+                    if (FinalTouchCoordinate.y > SwipeThreesold)
+                    {
+                        //Jump
+                        Debug.Log(myTouch + " swiping up");
+                    }
+                    else if (FinalTouchCoordinate.y < -SwipeThreesold)
+                    {
+                        //Return
+                        Debug.Log(myTouch + " swiping down");
+                    }
+                    break;
+            }
+        }
     }
 }
