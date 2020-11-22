d2204bac9c182ba556853534a2a4354d73ce0891
BernardTheFour <bernardctxy1@gmail.com>
2020-11-23 02:06:25 +0700

    Initiating Object Pooling

---
+++ Assets/Script/Platform/Controller/ObjectPooler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler SharedInstance;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    public List<GameObject> pooledObjects = null;
+    public List<GameObject> objectToPool = null;
+    public int amountToPool;
+
+    private void Start()
+    {
+        pooledObjects = new List<GameObject>();
+
+        foreach(GameObject obj in objectToPool)
+        {
+            for (int i = 0; i < amountToPool; i++)
+            {
+                GameObject instObj = (GameObject)Instantiate(obj);
+                instObj.SetActive(false);
+                pooledObjects.Add(instObj);
+            }
+        }
+    }
+}
