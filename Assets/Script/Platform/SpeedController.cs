c94f59e20201700972d181127778180c776a3fc4
BernardTheFour <bernardctxy1@gmail.com>
2020-11-28 17:23:00 +0700

    Speed Control Platform

---
+++ Assets/Script/Platform/SpeedController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour
+{
+    public static float Speed { private set; get; }
+
+    private static float playTime;
+
+    private float multiplier = 2;
+
+    private void Update()
+    {
+        playTime += Time.deltaTime;
+        Speed = CalculateSpeed(multiplier, playTime);
+    }
+
+    public static void Reset()
+    {
+        playTime = 0;
+    }
+
+    private float CalculateSpeed(float multiplier, float time)
+    {
+        float s = (2 / 3) * time + (2 * Mathf.Sin(time / 3)) + 5;
+        return s * multiplier;
+    }
+}
